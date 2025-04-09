f13e58d015ebe81a7016ffae1cabbeccd94f1e14
ashishbhatt77 <a7732808427@gmail.com>
2025-04-10 00:00:56 +0530

    first commitssssss

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "https://ecommerce-backend-9lah.onrender.com",
-        changeOrigin: true,
-        secure: false
-      }
-    }
+    port: 3000
   }
-});
+})
